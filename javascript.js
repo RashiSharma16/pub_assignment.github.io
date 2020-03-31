fa380c0879957e2c90b353e0fc58e7ff06337432
RashiSharma16 <1602raashi@gmail.com>
2020-03-31 16:44:39 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,7 +12,6 @@ function selectedImage(){
  }
 }
 
-
 function retrieveImg(){
 
   var img=document.createElement("img");
@@ -21,69 +20,46 @@ function retrieveImg(){
   return img;
 }
 
+function ValidateDOB() {
+        var lblError = document.getElementById("lblError");
+ 
+        //Get the date from the TextBox.
+        var dateString = document.getElementById("txtDate").value;
+        var regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
+ 
+        //Check whether valid dd/MM/yyyy Date Format.
+        if (regex.test(dateString)) {
+            var parts = dateString.split("/");
+            var dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
+            var dtCurrent = new Date();
+            lblError.innerHTML = "Eligibility 18 years ONLY."
+            if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
+                return false;
+            }
+ 
+            if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
+ 
+                //CD: 11/06/2018 and DB: 15/07/2000. Will turned 18 on 15/07/2018.
+                if (dtCurrent.getMonth() < dtDOB.getMonth()) {
+                    return false;
+                }
+                if (dtCurrent.getMonth() == dtDOB.getMonth()) {
+                    //CD: 11/06/2018 and DB: 15/06/2000. Will turned 18 on 15/06/2018.
+                    if (dtCurrent.getDate() < dtDOB.getDate()) {
+                        return false;
+                    }
+                }
+            }
+            lblError.innerHTML = "";
+            return true;
+        } else {
+            lblError.innerHTML = "Enter date in dd/MM/yyyy format ONLY."
+            return false;
+        }
+    }
 
-function validateForm() {
-  var nameVal,emailVal,mobileVal,dobVal;
-  var bt = document.getElementById('submit-btn');
 
-  // Get the value of the input field with id="numb"
-  nameVal = document.getElementById("name").value;
-  emailVal = document.getElementById("email").value;
-  mobileVal = document.getElementById("mobile").value;
-  dobVal = document.getElementById("dob").value;
 
-  // If nameVal is a string of alphabets more than 2 letters long and less than 30 letters long
-  if (nameVal!=="" && nameVal!=="undefined") {
-    var regex = /^[a-zA-Z ]{2,30}$/;
-    if (regex.test(nameVal.value)) {
-       bt.disabled=false;
-    }
-    else {
-         window.alert("Please enter only letters"); 
-        nameVal.focus(); 
-        bt.disabled=true;
-    }
-  }  
 
-//if email follows email pattern
-  if (emailVal!=="" && emailVal!=="undefined") {
-    var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-    if (regex.test(emailVal.value)) {
-       bt.disabled=false;
-    }
-    else {
-        window.alert("Please enter valid email address"); 
-        emailVal.focus(); 
-        bt.disabled=true;
-    }
-  }  
-
-//if mobile number contains 10 digits and does not start with 0
-  if (mobileVal!=="" && mobileVal!=="undefined") {
-    var regex = /^([1-9]{1}[0-9]{9}$/;
-    if (regex.test(emailVal.value)) {
-        bt.disabled=false;
-    }
-    else {
-         window.alert("Please enter valid mobile number"); 
-        mobileVal.focus(); 
-        bt.disabled=true;
-    }
-  }  
-
-//if date of birth 
-  if (dobVal!=="" && dobVal!=="undefined") {
-    var regex = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
-    if (regex.test(dob.value)) {
-        bt.disabled=false;
-    }
-    else {
-         window.alert("Please enter valid date of birth"); 
-        dobVal.focus(); 
-        bt.disabled=true;
-    }
-  }  
-
-  }
 
   
